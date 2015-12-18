d2170ab399f169bf08b9c05a24b38ddd5d02a497
ms <schmiddim@gmx.at>
2015-12-18 22:51:08 +0100

    db connection

---
+++ config/autoload/doctrine.dist.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	'doctrine' => array(
+		'connection' => array(
+			'orm_default' => array(
+				'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+				'params' => array(
+					'host' => 'localhost',
+					'port' => '3306',
+					'user' => 'root',
+					'password' => '',
+					'dbname' => 'zf2-cooking',
+				)))));
