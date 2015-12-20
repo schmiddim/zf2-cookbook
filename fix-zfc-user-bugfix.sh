65d34bd04a737388e1b92274bd35d5a20f5b062e
ms <schmiddim@gmx.at>
2015-12-20 15:31:54 +0100

    layout

---
+++ fix-zfc-user-bugfix.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+##there seems to be some bugs in zfc-user latest v1. so i fix it by going back
+
+cd /home/ms/code/php/zf2-cooking/vendor/zf-commons/zfc-user
+git checkout 1.4.1
